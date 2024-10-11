9a900c37e74bfe0545d8bc7adbff05b8c6d6dc35
hjvm2 <hjvm2@kent.ac.uk>
2024-10-11 11:40:03 +0000

    4

--- partFive.py
+++ partFive.py
@@ -1,1 +1,6 @@
+week = str(input("Enter day of the week: "))
 
+if week == 'saturday'or week == 'sunday':
+    print("Its the weekend!")
+else:
+    print("Its a weekday :(")
--- partFour.py
+++ partFour.py
@@ -1,1 +1,7 @@
+username = str(input("Enter username: "))
+password = str(input("Enter password: "))
 
+if username == 'admin' and password == 'password123':
+    print("Access Granted")
+else:
+    print("Access denied")
